e03c1317d948248ba547981f653365172a51e9cb
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2021-01-15 14:13:19 +0300

    Added mising @DefaultBean annotation to KafkaConsumerFactory.

--- eventuate-messaging-kafka-quarkus-consumer/src/main/java/io/eventuate/messaging/kafka/quarkus/consumer/KafkaConsumerFactoryConfiguration.java
+++ eventuate-messaging-kafka-quarkus-consumer/src/main/java/io/eventuate/messaging/kafka/quarkus/consumer/KafkaConsumerFactoryConfiguration.java
@@ -4,10 +4,12 @@ import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.inject.Produces;
 import io.eventuate.messaging.kafka.basic.consumer.DefaultKafkaConsumerFactory;
 import io.eventuate.messaging.kafka.basic.consumer.KafkaConsumerFactory;
+import io.quarkus.arc.DefaultBean;
 
 @ApplicationScoped
 public class KafkaConsumerFactoryConfiguration {
   @Produces
+  @DefaultBean
   public KafkaConsumerFactory kafkaConsumerFactory() {
     return new DefaultKafkaConsumerFactory();
   }
